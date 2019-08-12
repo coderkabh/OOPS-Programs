5d27f9f68b2eb322a48045fce196b7d9242551b4
Kanishk Bharadwaj <43510488+coderkabh@users.noreply.github.com>
2019-08-12 18:42:50 +0530

    Add files via upload

---
+++ 12Aug'19/TakingInputs.java
@@ -0,0 +1,6 @@
+public class TakingInputs {
+    public static void main(String[] args){
+    Scanner obj= new Scanner(System.in);
+
+    }
+}
---
+++ 12Aug'19/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args){
+        int age=19;
+        float roomTemperature=22.56f;
+        long mobileNumber=9634405898L;
+        char Gender='M';
+        // String name="Kanishk Bharadwaj";
+
+        //this was normal declaration and initialiation
+
+        System.out.println(age);
+        System.out.println(roomTemperature);
+        System.out.println(mobileNumber);
+        System.out.println(Gender);
+        //System.out.println(name);
+        Scanner obj = new Scanner(System.in);
+        int a=obj.nextInt();
+    }
+}
